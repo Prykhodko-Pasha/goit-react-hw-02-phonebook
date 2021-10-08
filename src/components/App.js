cf0586513fb1a3ddd9b29381b60f1eabaf7ac948
Prykhodko-Pasha <prykhodko.p@gmail.com>
2021-10-08 13:39:32 +0300

    finish

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,11 @@ import Section from './Section/Section';
 import Form from './Form/Form';
 import Contacts from './Contacts/Contacts';
 import ContactsSearch from './Contacts/ContactsSearch';
+import s from './Contacts/Contacts.module.css';
 
 export default class Phonebook extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -60,7 +56,9 @@ export default class Phonebook extends React.Component {
               />
             </>
           ) : (
-            <p>No contacts yet</p>
+            <div className={s.wrapper}>
+              <p className={s.text}>No contacts yet</p>
+            </div>
           )}
         </Section>
       </>
